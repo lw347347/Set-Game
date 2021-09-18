97d5b4e75d843e2f2237afa4c9dfcc9ccf24e1df
Landon Williams <landonw347@gmail.com>
2021-09-17 20:14:27 -0600

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Landon Williams on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
